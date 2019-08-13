cd1812ed5f45e00f1e4673560ef03c11a0b05530
rbrenor2 <rbrenorios@gmail.com>
2019-08-13 17:42:48 -0300

    base files

---
+++ lib/ui/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          'Wallet',
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Colors.black,
+            fontWeight: FontWeight.w600
+          ),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+
+}
